5f0ed1e4380190b5bb0f2942e6c5abb483a3b57b
Anton Herbanosenka <dr.herbanosenka@gmail.com>
2022-10-15 14:58:40 +0300

    feat: done carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity != "string") return false;
+  if (!isNaN(sampleActivity) && !isNaN(parseFloat(sampleActivity))) {
+    if (+sampleActivity > 15 || +sampleActivity <= 0) return false;
+    let k = 0.69314718056 / HALF_LIFE_PERIOD;
+    return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / k);
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
